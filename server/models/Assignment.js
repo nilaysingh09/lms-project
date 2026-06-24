4918be03706914c0541e429b65ade174ef67f73e
Nilay <nilay109singh@gmail.com>
2026-06-24 19:23:21 +0530

    Updated Assignment.js

---
+++ server/models/Assignment.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+
+const assignmentSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true
+  },
+  description: {
+    type: String,
+    required: true
+  },
+  course: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Course',
+    required: true
+  },
+  dueDate: {
+    type: Date,
+    required: true
+  },
+  totalMarks: {
+    type: Number,
+    default: 100
+  }
+}, { timestamps: true });
+
+module.exports = mongoose.model('Assignment', assignmentSchema);
