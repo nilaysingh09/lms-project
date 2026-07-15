e4e2e8f8ed1ca117d397de18d2e4e17544bce7b9
Nilay <nilay109singh@gmail.com>
2026-07-15 17:44:13 +0530

    Updated Quiz

--- server/controllers/quizController.js
+++ server/controllers/quizController.js
@@ -111,4 +111,18 @@ const getMyResults = async (req, res) => {
   }
 };
 
-module.exports = { createQuiz, getQuizzesByCourse, submitQuiz, getMyResults };
+// Get single quiz by ID
+const getQuizById = async (req, res) => {
+  try {
+    const quiz = await Quiz.findById(req.params.quizId)
+      .select('-questions.correctAnswer');
+    if (!quiz) {
+      return res.status(404).json({ message: 'Quiz not found' });
+    }
+    res.status(200).json(quiz);
+  } catch (error) {
+    res.status(500).json({ message: 'Server error', error: error.message });
+  }
+};
+
+module.exports = { createQuiz, getQuizzesByCourse, submitQuiz, getMyResults, getQuizById };
